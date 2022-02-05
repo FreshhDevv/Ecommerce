9fefb841a36ff79106d80de47bd1820c4e305037
FreshDev <billyhans90@gmail.com>
2022-02-05 07:18:55 +0100

    Fixed the front end bug Changed the footer, copy rights

--- ecommerce/resources/views/admin/admin_master.blade.php
+++ ecommerce/resources/views/admin/admin_master.blade.php
@@ -47,7 +47,7 @@
   	
 	 
 	<!-- Vendor JS -->
-	<script src="{{ asset('js/vendors.min.js') }}"></script>
+	<script src="{{ asset('backend/js/vendors.min.js') }}"></script>
     <script src="{{ asset('../assets/icons/feather-icons/feather.min.js') }}"></script>	
 	<script src="{{ asset('../assets/vendor_components/easypiechart/dist/jquery.easypiechart.js') }}"></script>
 	<script src="{{ asset('../assets/vendor_components/apexcharts-bundle/irregular-data-series.js') }}"></script>
