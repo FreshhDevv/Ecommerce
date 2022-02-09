dc36ec51ed90bcc979bd8602baf7cff93d12032c
FreshDev <billyhans90@gmail.com>
2022-02-09 05:24:22 +0100

    Display selected profile picture on the dashboard using jquery

--- ecommerce/resources/views/admin/admin_profile_edit.blade.php
+++ ecommerce/resources/views/admin/admin_profile_edit.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 <div class="container-full">
 
@@ -48,16 +49,16 @@
                                 <div class="form-group">
 								<h5>Profile Image<span class="text-danger">*</span></h5>
 								<div class="controls">
-									<input type="file" name="profile_photo_path" class="form-control" required=""> <div class="help-block"></div></div>
+									<input type="file" name="profile_photo_path" class="form-control" required="" id="image"></div>
 							</div>
 
                                 </div>
 
                                 <div class="col-md-6">
 
-                                <img src="{{ (!empty($adminData->profile_photo_path))? 
+                                <img id="showImage" src="{{ (!empty($adminData->profile_photo_path))? 
                         url('upload/admin_images'.$adminData->profile_photo_path):
-                        url('upload/no_image.jpg') }}" style=""width: 100px; height="100px"> 
+                        url('upload/no_image.jpg') }}" style="width: 100px; height: 100px"> 
                                     
                                     </div>
                             </div>
@@ -87,4 +88,17 @@
    
 
 </div>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#image').change(function(e) {
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                $('#showImage').attr('src',e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+</script>
+
 @endsection
